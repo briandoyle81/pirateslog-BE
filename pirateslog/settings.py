71575bdb1419600968c260e3d40f9a39a599d93c
briandoyle81 <briandoyle81@gmail.com>
2019-05-17 13:32:45 -0600

    changed allowed hosts

--- pirateslog/settings.py
+++ pirateslog/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [host.strip() for host in config('ALLOWED_HOSTS').split(',')]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
