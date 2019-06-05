943e14c950972869a9532e0c56337e21a481a456
briandoyle81 <briandoyle81@gmail.com>
2019-06-05 11:22:22 -0600

    testing if switching order of permission classes fixes error

--- pirateslog/settings.py
+++ pirateslog/settings.py
@@ -195,8 +195,8 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
