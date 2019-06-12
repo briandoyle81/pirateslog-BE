8a9def31123f30d52a1e4a44f349c58735798a12
briandoyle81 <briandoyle81@gmail.com>
2019-06-12 11:38:35 -0600

    this is why we test

--- log/views.py
+++ log/views.py
@@ -70,7 +70,7 @@ def verify_gamertag(request):
     if dbProfile.verificationCode == request.data.get('code'):
         print("code matches")
         dbProfile.verified = True
-        dbProfile.safe()
+        dbProfile.save()
         return(Response("true"))
     return(Response("false"))
 
